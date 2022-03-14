aa91b5b87c31840979ff3e0e9094d6bdc13188df
Agbamajnr <ulimhunyieagbama@gmail.com>
2022-03-14 22:05:21 +0100

    alignment for for navmenu

--- src/components/NavShow.vue
+++ src/components/NavShow.vue
@@ -1,18 +1,18 @@
 <template>
 <div class="main grid-center w-full h-full py-8" ref="main">
-      <div class="body col-flex w-full px-16 h-full">
+      <div class="body col-flex w-full px-10 sm:px-16 h-full">
           <div class="row-flex items-center justify-between">
               <Logo/>
               <img src="../assets/img/navClose.svg" class="cursor-pointer" alt="" @click="closeNav">
           </div>
-          <div class="details row-flex h-full items-center justify-between sm:pt-15 lg:pt-20">
-              <div class="links col-flex h-full pr-4" style="width: 43%">
-                <div class="group1 flex flex-row justify-between w-full">
+          <div class="details flex flex-col sm:flex-row h-full sm:items-center justify-between  sm:justify-between sm:pt-15 lg:pt-20 mt-10 sm:mt-0">
+              <div class="links col-flex h-full pr-4 w-full sm:w-3/6">
+                <div class="group1 flex flex-col sm:flex-row justify-between w-full">
                     <div class="section">
-                    <p class="title font-bold text-xl">Menu</p>
-                    <p class="pt-3">Home</p>
-                    <p class="pt-1 ">Bid Shop</p>
-                    <p class="pt-1">Products</p>
+                        <p class="title font-bold text-xl">Menu</p>
+                        <p class="pt-3">Home</p>
+                        <p class="pt-1 ">Bid Shop</p>
+                        <p class="pt-1">Products</p>
                     </div>
                     <div class="section">
                         <p class="title font-bold text-xl">Profile</p>
@@ -29,7 +29,7 @@
                         <p class="pt-1">Roadmap</p>
                     </div>
                 </div>
-                <div class="group2 row-flex justify-between w-1/2">
+                <div class="group2 flex flex-col sm:flex-row justify-between sm:w-1/2">
                     <div class="section">
                     <p class="title font-bold text-xl">Loans</p>
                     <p class="pt-3">Buyers Loans</p>
@@ -44,39 +44,39 @@
                     </div>
                 </div>
               </div>
-              <div class="quickService h-full col-flex text-black gap-3" style="width: 57%">
+              <div class="quickService h-full flex flex-col justify-between text-black sm:gap-3 sm:w-4/6 mt-40 sm:mt-0">
                     <p class="font-bold">Quick Service</p>
-                    <div class="top row-flex h-2/5 w-full mt-3 justify-between gap-4 ">
+                    <div class="top flex sm:flex-row flex-col sm:h-2/5 w-full mt-3 justify-between sm:gap-4 ">
                         <div class="card h-full w-full">
                             <p class="title font-bold text-lg">Logistics</p>
                             <p class="sub">N500 per Kg</p>
                         </div>
-                        <div class="card h-full w-full">
+                        <div class="card h-full w-full mt-4 sm:mt-0">
                             <p class="title font-bold text-lg">Logistics</p>
                             <p class="sub">N500 per Kg</p>
                         </div>
-                        <div class="card w-full h-full">
+                        <div class="card w-full h-full mt-4 sm:mt-0">
                             <p class="title font-bold text-lg">Logistics</p>
                             <p class="sub">N500 per Kg</p>
                         </div>
                     </div>
-                    <div class="bottom h-3/5 w-full row-flex text-black gap-x-3">
-                        <div class="left w-4/5 col-flex gap-y-3">
-                            <div class="top w-full bg-blue-300 grid place-items-center rounded" style="height: 48%;">
+                    <div class="bottom sm:h-3/5 w-full flex sm:flex-row flex-col text-black gap-x-3">
+                        <div class="left w-full sm:w-4/5 col-flex gap-y-3">
+                            <div class="top w-full bg-blue-300 grid place-items-center rounded mt-4 sm:mt-0" style="height: 48%;">
                                 <p>Payment Methods</p>
                             </div>
-                            <div class="bottom w-full rounded row-flex gap-x-3" style="height: 48%;">
-                                <div class="h-full w-1/2 bg-green-200 rounded"></div>
-                                <div class="h-full w-1/2 bg-green-500 rounded"></div>
+                            <div class="bottom w-full rounded flex sm:flex-row flex-col gap-x-3" style="height: 48%;">
+                                <div class="card h-full w-full sm:w-1/2 bg-green-200 rounded "></div>
+                                <div class="card h-full w-full sm:w-1/2 bg-green-500 rounded mt-4 sm:mt-0"></div>
                             </div>
                         </div>
-                        <div class="right h-full w-2/5 col-flex justify-between text-black rounded overflow-hidden" style="background: #F6F1FC;">
+                        <!-- <div class="right h-full w-2/5 col-flex justify-between text-black rounded overflow-hidden" style="background: #F6F1FC;">
                             <div class="grid-center text-black h-1/2">
                                 <p class="font-bold  text-black">Quick support</p>
                                 <p class="text-black">Single click support team</p>
                             </div>
                             <img src="../assets/img/character.svg" alt="">
-                        </div>
+                        </div> -->
                     </div>
               </div>
           </div>
@@ -105,9 +105,8 @@ export default {
 <style scoped>
     .main {
         background-color: #000;
-    }
-    .links {
-        overflow: auto;
+        height: 100%;
+        overflow: auto
     }
     .section p {
         font-size: 14px;
@@ -120,5 +119,10 @@ export default {
         justify-content: center;
         flex-direction: column;
         border-radius: 5px;
+    }
+    @media (max-width: 768px) {
+        .card {
+            min-height: 140px;
+        }
     }
 </style>
